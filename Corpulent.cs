6454dd9338de6281584fda67d01410c86415ac95
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2021-06-21 00:33:16 -0400

    Fix weird jolly co-op incompat

--- Corpulent.cs
+++ Corpulent.cs
@@ -1,4 +1,5 @@
 ﻿using RWCustom;
+using System;
 using UnityEngine;
 
 namespace GameruleSet
@@ -11,7 +12,20 @@ namespace GameruleSet
         {
             this.rules = rules;
 
+            On.SaveState.SaveToString += SaveState_SaveToString;
             On.SlugcatStats.SlugcatFoodMeter += SlugcatStats_SlugcatFoodMeter;
+        }
+
+        private string SaveState_SaveToString(On.SaveState.orig_SaveToString orig, SaveState self)
+        {
+            var tempFood = self.food;
+            if (self.progression.rainWorld.processManager.currentMainLoop is RainWorldGame g && g.Players.Count > 0 && g.Players[0].realizedCreature is Player p)
+            {
+                self.food = Mathf.Clamp(p.playerState.foodInStomach - p.slugcatStats.foodToHibernate, 0, p.slugcatStats.maxFood);
+            }
+            var ret = orig(self);
+            self.food = tempFood;
+            return ret;
         }
 
         private IntVector2 SlugcatStats_SlugcatFoodMeter(On.SlugcatStats.orig_SlugcatFoodMeter orig, int slugcatNum)
