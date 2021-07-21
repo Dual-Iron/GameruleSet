76bf87a50480bd259ee541ae981543e295945969
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2021-07-21 02:41:26 -0400

    Limit shared grasps to players
    Prevents, e.g., tamed lizards grabbing an item you're holding and pulling you into a den (killing you).

--- ShareGrasps.cs
+++ ShareGrasps.cs
@@ -12,17 +12,26 @@
             On.Creature.Grasp.ShareabilityConflict += Grasp_ShareabilityConflict;
         }
 
-        private int chunkGrabbed;
+        private int cachedChunkGrabbed;
+        private Creature? cachedGrabber;
 
         private bool Creature_Grab(On.Creature.orig_Grab orig, Creature self, PhysicalObject obj, int graspUsed, int chunkGrabbed, Creature.Grasp.Shareability shareability, float dominance, bool overrideEquallyDominant, bool pacifying)
         {
-            this.chunkGrabbed = chunkGrabbed;
-            return orig(self, obj, graspUsed, chunkGrabbed, shareability, dominance, overrideEquallyDominant, pacifying);
+            cachedChunkGrabbed = chunkGrabbed;
+            cachedGrabber = self;
+            var ret = orig(self, obj, graspUsed, chunkGrabbed, shareability, dominance, overrideEquallyDominant, pacifying);
+            cachedChunkGrabbed = default;
+            cachedGrabber = default;
+            return ret;
         }
 
         private bool Grasp_ShareabilityConflict(On.Creature.Grasp.orig_ShareabilityConflict orig, Creature.Grasp self, Creature.Grasp.Shareability other)
         {
-            return (self.grabber is not Player || !rules.ShareGrasps || self.chunkGrabbed == chunkGrabbed) && orig(self, other);
+            if (rules.ShareGrasps && cachedGrabber is Player && self.grabber is Player && self.chunkGrabbed != cachedChunkGrabbed)
+            {
+                return false;
+            }
+            return orig(self, other);
         }
     }
 }
