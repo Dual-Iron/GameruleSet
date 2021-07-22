93721024fe734f0ccec6ee9c02721e427c3a6bc4
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2021-07-22 14:05:02 -0400

    Bump version

--- GameruleSetPlugin.cs
+++ GameruleSetPlugin.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace GameruleSet
 {
-    [BepInPlugin("com.github.dual.gameruleset", "Gamerule Set", "1.5.0")]
+    [BepInPlugin("com.github.dual.gameruleset", "Gamerule Set", "1.5.1")]
     public sealed class GameruleSetPlugin : BaseUnityPlugin
     {
         public Rules? Gamerules { get; private set; }
